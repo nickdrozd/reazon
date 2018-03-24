40a037e90493cae2591ed0cd06c2946cb696240c
Nick Drozd <nicholasdrozd@gmail.com>
2018-03-24 13:56:30 -0500

    Add variable functions

--- reason.el
+++ reason.el
@@ -1,1 +1,15 @@
 ;; -*- lexical-binding: t -*-
+
+;; variables
+
+(defun reason-make-variable (name)
+  "Create a unique variable from NAME."
+  (vector name))
+
+(defun reason-variable-p (var)
+  "Check whether VAR is a variable."
+  (vector-or-char-table-p var))
+
+(ert-deftest reason-variable-test ()
+  (should (reason-variable-p (reason-make-variable 'x)))
+  (should-not (reason-variable-p 'x)))
