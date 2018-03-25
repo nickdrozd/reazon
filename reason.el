3e91425cefe7bbb89cfcb5625ad2c2261c5905c2
Nick Drozd <nicholasdrozd@gmail.com>
2018-03-25 12:38:13 -0500

    Add reason-with-variables
    I expect this to be used mostly for testing, but maybe it will have
    application elsewhere.

--- reason.el
+++ reason.el
@@ -10,6 +10,20 @@
   "Check whether VAR is a variable."
   (vector-or-char-table-p var))
 
+(defmacro reason-with-variables (variables &rest body)
+  "Evaluate BODY with VARIABLES as reason-variables."
+  (declare (indent 1))
+  (let ((reason-vars
+         (mapcar (lambda (var)
+                   `(,var (reason-make-variable ',var)))
+                 variables)))
+    `(let (,@reason-vars)
+       ,@body)))
+
 (ert-deftest reason-variable-test ()
   (should (reason-variable-p (reason-make-variable 'x)))
-  (should-not (reason-variable-p 'x)))
+  (should-not (reason-variable-p 'x))
+  (reason-with-variables (x y z)
+    (should (reason-variable-p x))
+    (should (reason-variable-p y))
+    (should (reason-variable-p z))))
