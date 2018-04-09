5f409a826992935b623672428f6016460537d2b2
Nick Drozd <nicholasdrozd@gmail.com>
2018-04-08 23:09:06 -0500

    Add header and footer

--- reason.el
+++ reason.el
@@ -1,4 +1,28 @@
-;; -*- lexical-binding: t -*-
+;;; reason.el --- miniKanren implementation for Emacs  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018  Nick Drozd
+
+;; Author: Nick Drozd <nicholasdrozd@gmail.com>
+;; Keywords: languages, extensions, lisp
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
 
 ;; variables
 
@@ -186,3 +210,7 @@ SUBSTITUTION if there is one, else VARIABLE."
       (reason-should-equal
        (funcall (reason-reify x) `(,a1 ,a2 ,a3))
        `(_0 (_1 _0) corn _2 ((ice) _2))))))
+
+
+(provide 'reason)
+;;; reason.el ends here
