abd5922be72daecbee65428c41880551245c0e2a
Nick Drozd <nicholasdrozd@gmail.com>
2018-09-19 20:03:51 -0500

    Use 9x9 for benchmark
    Now that 4x4 is too easy, it's time to move on to something harder.
    This will be convenient because difficulty can be ratcheted up
    gradually by removing one square at a time.
    
    Here are some times running on a 2012 MBP:
    
    Elapsed time: 79.283037s (29.438492s in 319 GCs)
    Elapsed time: 76.251785s (28.548067s in 319 GCs)
    Elapsed time: 73.628577s (27.994638s in 318 GCs)
    Elapsed time: 80.264210s (31.539141s in 358 GCs)
    Elapsed time: 79.808532s (31.391441s in 357 GCs)
    Elapsed time: 81.097268s (29.375902s in 319 GCs)
    
    The specific times are fairly unstable, though they all fall in the
    70-80-ish range. What's interesting is how little time is spent in GC
    as compared with the previous benchmark. Why is this?

--- reazon-perf.el
+++ reazon-perf.el
@@ -25,6 +25,7 @@
 ;;; Code:
 
 (require 'benchmark)
+(require 'cl-lib)
 (require 'profiler)
 (require 'reazon)
 (require 'reazon-sudoku)
@@ -45,7 +46,31 @@ Keep an eye out for recursive functions!"
 (defun reazon-benchmark ()
   "Benchmark Reazon performance."
   (interactive)
-  (benchmark 1 '(reazon-sudoku-solve-4x4)))
+  (benchmark
+   1
+   '(let ((board
+           (reazon-sudoku-solve-9x9
+                   (a2 3) (a3 5) (a4 2) (a5 6) (a6 9) (a7 7)
+            (b1 6)        (b3 2) (b4 5)        (b6 1) (b7 4)        (b9 3)
+            (c1 1) (c2 9)        (c4 8) (c5 3) (c6 4)        (c8 6) (c9 2)
+            (d1 8) (d2 2) (d3 6)        (d5 9)        (d7 3)        (d9 7)
+                   (e2 7) (e3 4) (e4 6)        (e6 2) (e7 9) (e8 1) (e9 5)
+            (f1 9) (f2 5) (f3 1)        (f5 4)        (f7 6) (f8 2)
+                   (g2 1)        (g4 3) (g5 2) (g6 6)        (g8 7)
+            (h1 2)        (h3 8) (h4 9) (h5 5) (h6 7) (h7 1)
+                   (i2 6) (i3 3))))
+      (cl-assert
+       (equal
+        board
+        '((4 3 5 2 6 9 7 8 1
+             6 8 2 5 7 1 4 9 3
+             1 9 7 8 3 4 5 6 2
+             8 2 6 1 9 5 3 4 7
+             3 7 4 6 8 2 9 1 5
+             9 5 1 7 4 3 6 2 8
+             5 1 9 3 2 6 8 7 4
+             2 4 8 9 5 7 1 3 6
+             7 6 3 4 1 8 2 5 9)))))))
 
 
 (provide 'reazon-perf)
