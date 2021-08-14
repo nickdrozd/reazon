1bfce512612eb679d06356b5c070e6f8743fd0dc
Nick Drozd <nicholasdrozd@gmail.com>
2021-08-14 11:14:47 -0500

    Disable occurs-check for 9x9 sudoku
    Test time on my crappy little Thinkpad:
    
      - Before: 17.385282 sec
      -  After: 10.225427 sec

--- reazon-sudoku.el
+++ reazon-sudoku.el
@@ -109,47 +109,48 @@ will be generated. In particular, if no constraints are specified, all 9x9
 solved instances will be generated (but don't rely on it; the universe will
 expire before the computation completes)."
 
-  `(reazon-run* (a1 a2 a3 a4 a5 a6 a7 a8 a9
-                    b1 b2 b3 b4 b5 b6 b7 b8 b9
-                    c1 c2 c3 c4 c5 c6 c7 c8 c9
-                    d1 d2 d3 d4 d5 d6 d7 d8 d9
-                    e1 e2 e3 e4 e5 e6 e7 e8 e9
-                    f1 f2 f3 f4 f5 f6 f7 f8 f9
-                    g1 g2 g3 g4 g5 g6 g7 g8 g9
-                    h1 h2 h3 h4 h5 h6 h7 h8 h9
-                    i1 i2 i3 i4 i5 i6 i7 i8 i9)
+  `(let (reazon-occurs-check)
+     (reazon-run* (a1 a2 a3 a4 a5 a6 a7 a8 a9
+                      b1 b2 b3 b4 b5 b6 b7 b8 b9
+                      c1 c2 c3 c4 c5 c6 c7 c8 c9
+                      d1 d2 d3 d4 d5 d6 d7 d8 d9
+                      e1 e2 e3 e4 e5 e6 e7 e8 e9
+                      f1 f2 f3 f4 f5 f6 f7 f8 f9
+                      g1 g2 g3 g4 g5 g6 g7 g8 g9
+                      h1 h2 h3 h4 h5 h6 h7 h8 h9
+                      i1 i2 i3 i4 i5 i6 i7 i8 i9)
 
-     ,@(mapcar
-        (lambda (cvp) `(reazon-== ,@cvp))
-        coordinate-value-pairs)
+       ,@(mapcar
+          (lambda (cvp) `(reazon-== ,@cvp))
+          coordinate-value-pairs)
 
-     (reazon--in-range-9 (list a1 a2 a3 a4 a5 a6 a7 a8 a9))
-     (reazon--in-range-9 (list a1 b1 c1 d1 e1 f1 g1 h1 i1))
-     (reazon--in-range-9 (list a1 a2 a3 b1 b2 b3 c1 c2 c3))
-     (reazon--in-range-9 (list a2 b2 c2 d2 e2 f2 g2 h2 i2))
-     (reazon--in-range-9 (list b1 b2 b3 b4 b5 b6 b7 b8 b9))
-     (reazon--in-range-9 (list a3 b3 c3 d3 e3 f3 g3 h3 i3))
-     (reazon--in-range-9 (list c1 c2 c3 c4 c5 c6 c7 c8 c9))
-     (reazon--in-range-9 (list a4 a5 a6 b4 b5 b6 c4 c5 c6))
-     (reazon--in-range-9 (list a4 b4 c4 d4 e4 f4 g4 h4 i4))
-     (reazon--in-range-9 (list a5 b5 c5 d5 e5 f5 g5 h5 i5))
-     (reazon--in-range-9 (list d1 d2 d3 d4 d5 d6 d7 d8 d9))
-     (reazon--in-range-9 (list d4 d5 d6 e4 e5 e6 f4 f5 f6))
-     (reazon--in-range-9 (list d1 d2 d3 e1 e2 e3 f1 f2 f3))
-     (reazon--in-range-9 (list e1 e2 e3 e4 e5 e6 e7 e8 e9))
-     (reazon--in-range-9 (list f1 f2 f3 f4 f5 f6 f7 f8 f9))
-     (reazon--in-range-9 (list a6 b6 c6 d6 e6 f6 g6 h6 i6))
-     (reazon--in-range-9 (list g4 g5 g6 h4 h5 h6 i4 i5 i6))
-     (reazon--in-range-9 (list i1 i2 i3 i4 i5 i6 i7 i8 i9))
-     (reazon--in-range-9 (list g1 g2 g3 g4 g5 g6 g7 g8 g9))
-     (reazon--in-range-9 (list h1 h2 h3 h4 h5 h6 h7 h8 h9))
-     (reazon--in-range-9 (list a7 b7 c7 d7 e7 f7 g7 h7 i7))
-     (reazon--in-range-9 (list a8 b8 c8 d8 e8 f8 g8 h8 i8))
-     (reazon--in-range-9 (list a9 b9 c9 d9 e9 f9 g9 h9 i9))
-     (reazon--in-range-9 (list a7 a8 a9 b7 b8 b9 c7 c8 c9))
-     (reazon--in-range-9 (list d7 d8 d9 e7 e8 e9 f7 f8 f9))
-     (reazon--in-range-9 (list g1 g2 g3 h1 h2 h3 i1 i2 i3))
-     (reazon--in-range-9 (list g7 g8 g9 h7 h8 h9 i7 i8 i9))))
+       (reazon--in-range-9 (list a1 a2 a3 a4 a5 a6 a7 a8 a9))
+       (reazon--in-range-9 (list a1 b1 c1 d1 e1 f1 g1 h1 i1))
+       (reazon--in-range-9 (list a1 a2 a3 b1 b2 b3 c1 c2 c3))
+       (reazon--in-range-9 (list a2 b2 c2 d2 e2 f2 g2 h2 i2))
+       (reazon--in-range-9 (list b1 b2 b3 b4 b5 b6 b7 b8 b9))
+       (reazon--in-range-9 (list a3 b3 c3 d3 e3 f3 g3 h3 i3))
+       (reazon--in-range-9 (list c1 c2 c3 c4 c5 c6 c7 c8 c9))
+       (reazon--in-range-9 (list a4 a5 a6 b4 b5 b6 c4 c5 c6))
+       (reazon--in-range-9 (list a4 b4 c4 d4 e4 f4 g4 h4 i4))
+       (reazon--in-range-9 (list a5 b5 c5 d5 e5 f5 g5 h5 i5))
+       (reazon--in-range-9 (list d1 d2 d3 d4 d5 d6 d7 d8 d9))
+       (reazon--in-range-9 (list d4 d5 d6 e4 e5 e6 f4 f5 f6))
+       (reazon--in-range-9 (list d1 d2 d3 e1 e2 e3 f1 f2 f3))
+       (reazon--in-range-9 (list e1 e2 e3 e4 e5 e6 e7 e8 e9))
+       (reazon--in-range-9 (list f1 f2 f3 f4 f5 f6 f7 f8 f9))
+       (reazon--in-range-9 (list a6 b6 c6 d6 e6 f6 g6 h6 i6))
+       (reazon--in-range-9 (list g4 g5 g6 h4 h5 h6 i4 i5 i6))
+       (reazon--in-range-9 (list i1 i2 i3 i4 i5 i6 i7 i8 i9))
+       (reazon--in-range-9 (list g1 g2 g3 g4 g5 g6 g7 g8 g9))
+       (reazon--in-range-9 (list h1 h2 h3 h4 h5 h6 h7 h8 h9))
+       (reazon--in-range-9 (list a7 b7 c7 d7 e7 f7 g7 h7 i7))
+       (reazon--in-range-9 (list a8 b8 c8 d8 e8 f8 g8 h8 i8))
+       (reazon--in-range-9 (list a9 b9 c9 d9 e9 f9 g9 h9 i9))
+       (reazon--in-range-9 (list a7 a8 a9 b7 b8 b9 c7 c8 c9))
+       (reazon--in-range-9 (list d7 d8 d9 e7 e8 e9 f7 f8 f9))
+       (reazon--in-range-9 (list g1 g2 g3 h1 h2 h3 i1 i2 i3))
+       (reazon--in-range-9 (list g7 g8 g9 h7 h8 h9 i7 i8 i9)))))
 
 
 (provide 'reazon-sudoku)
